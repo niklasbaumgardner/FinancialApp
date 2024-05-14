23c984ac9a4a3613c7e7c1a54c54f0e7f36f3478
niklasbaumgardner <niklasbaumgardner@gmail.com>
2024-05-14 12:16:17 -0400

    use current_user in profile.py

--- finapp/routes/profile.py
+++ finapp/routes/profile.py
@@ -8,12 +8,13 @@ profile_bp = Blueprint("profile_bp", __name__)
 @profile_bp.route("/profile", methods=["GET", "POST"])
 @login_required
 def profile():
-    user = user_queries.get_user_by_id(id=current_user.id)
     if request.method == "POST":
         username = request.form.get("username")
         email = request.form.get("email")
 
-        user_queries.update_user(id=user.id, email=email, username=username)
+        user_queries.update_user(id=current_user.id, email=email, username=username)
 
         return redirect(url_for("profile_bp.profile"))
-    return render_template("profile.html", username=user.username, email=user.email)
+    return render_template(
+        "profile.html", username=current_user.username, email=current_user.email
+    )
