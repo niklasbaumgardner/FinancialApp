7e0a031e5e115fbeddb2bc9cf9e72b1e6c535e74
niklasbaumgardner <niklasbaumgardner@gmail.com>
2024-08-01 16:04:34 -0400

    count pages correctly

--- finapp/queries/transaction_queries.py
+++ finapp/queries/transaction_queries.py
@@ -13,7 +13,7 @@ from datetime import date
 
 def paginate_query(query, page):
     # transactions = transactions.paginate(page=page, per_page=10)
-    num_pages = (query.count() // 10) + 1
+    num_pages = max(1, ((query.count() - 1) // 10) + 1)
     query = query.limit(10).offset((page - 1) * 10).all()
     return (
         query,
