fe56fa21fe1049765ac95e6e76cb71132aa3e978
niklasbaumgardner <niklasbaumgardner@gmail.com>
2025-06-24 10:20:08 -0400

    update session cookie name and domain

--- finapp/__init__.py
+++ finapp/__init__.py
@@ -28,7 +28,7 @@ if not os.environ.get("FLASK_DEBUG"):
             # possible.
             "continuous_profiling_auto_start": True,
         },
-        release="nbfinancial@1.0.8",
+        release="nbfinancial@1.0.9",
     )
 
 
