1c68872c4ec45ae645e799175583f9eb788a5a41
niklasbaumgardner <niklasbaumgardner@gmail.com>
2025-06-24 10:49:34 -0400

    remove unneeded queries

--- finapp/routes/viewtransactions.py
+++ finapp/routes/viewtransactions.py
@@ -14,15 +14,6 @@ viewtransactions_bp = Blueprint("viewtransactions_bp", __name__)
 @viewtransactions_bp.get("/view_transactions")
 @login_required
 def view_transactions():
-    transactions = transaction_queries.get_recent_transactions()
-    transactions = [t.to_dict() for t in transactions]
-
-    budgets = budget_queries.get_budgets(active_only=True)
-    budgets = [b.to_dict() for b in budgets]
-
-    categories = category_queries.get_categories_shared()
-    categories = [c.to_dict() for c in categories]
-
     return render_template(
         "viewtransactions.html",
     )
