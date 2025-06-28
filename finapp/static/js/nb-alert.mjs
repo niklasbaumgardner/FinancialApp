ea19234cc6a4acc1ad87742d1518b7813b308fc0
niklasbaumgardner <niklasbaumgardner@gmail.com>
2025-06-27 22:02:54 -0400

    fix callout class so they are alerts now

---
+++ finapp/static/js/nb-alert.mjs
@@ -0,0 +1,34 @@
+import { NikElement } from "./customElement.mjs";
+import { html } from "./imports.mjs";
+
+export class Alert extends NikElement {
+  static properties = {
+    message: { type: String },
+    category: { type: String },
+  };
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    this.timeoutId = setTimeout(() => this.removeSelf(), 5000);
+  }
+
+  removeSelf() {
+    clearTimeout(this.timeoutId);
+    this.remove();
+  }
+
+  render() {
+    return html`<wa-callout variant=${this.category} closable>
+      ${this.message}
+      <wa-icon-button
+        name="system/close-large-line"
+        library="remix"
+        label="Remove"
+        @click=${this.removeSelf}
+      ></wa-icon-button>
+    </wa-callout>`;
+  }
+}
+
+customElements.define("nb-alert", Alert);
