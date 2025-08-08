1263a4116498e23ab3b3d00d487838d61a776331
niklasbaumgardner <niklasbaumgardner@gmail.com>
2025-08-08 15:40:06 -0400

    add ProxyFix

--- finapp/__init__.py
+++ finapp/__init__.py
@@ -9,6 +9,7 @@ from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.pool import NullPool
 import sentry_sdk
 import os
+from werkzeug.middleware.proxy_fix import ProxyFix
 # from flask_compress import Compress
 # from flask_caching import Cache
 # from sqlalchemy import event
@@ -23,13 +24,15 @@ class Base(DeclarativeBase):
 
 app = Flask(__name__)
 
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
+
 
 if not os.environ.get("FLASK_DEBUG"):
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_DSN"),
         traces_sample_rate=1.0,
         _experiments={"enable_logs": True},
-        release="nb-budgets@1.1.17",
+        release="nb-budgets@1.1.18",
     )
 
 
