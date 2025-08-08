5b7ae396d4e00e7fc171050b0ec316f3f35fb6e1
niklasbaumgardner <niklasbaumgardner@gmail.com>
2025-08-08 15:31:55 -0400

    back to PORT

--- flask_start.sh
+++ flask_start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-
-# export PORT=${PORT}
-
 echo using port: ${PORT}
 
-exec gunicorn run:app -k gevent --workers 17 -b [::]:${3000:-3000} --log-level debug
+exec gunicorn run:app -k gevent --workers 17 -b [::]:${PORT:-3000} --log-level debug
