02aec383b42823ce85481e48d982ad6c3681a533
niklasbaumgardner <niklasbaumgardner@gmail.com>
2025-08-11 17:23:35 -0400

    preconnect to external urls

--- finapp/__init__.py
+++ finapp/__init__.py
@@ -32,7 +32,7 @@ if not os.environ.get("FLASK_DEBUG"):
         dsn=os.environ.get("SENTRY_DSN"),
         traces_sample_rate=1.0,
         _experiments={"enable_logs": True},
-        release="nb-budgets@1.1.18",
+        release="nb-budgets@1.1.19",
     )
 
 
