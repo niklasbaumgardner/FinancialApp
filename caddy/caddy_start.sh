4bfcc8c0b5e1d6537f6eb88f6f461e635498c4cb
niklasbaumgardner <niklasbaumgardner@gmail.com>
2025-08-11 17:14:13 -0400

    remove FRONTEND_HOST

--- caddy/caddy_start.sh
+++ caddy/caddy_start.sh
@@ -3,12 +3,12 @@
 set -euo pipefail
 
 # for backwards compatibility, seperates host and port from url
-export APP_DOMAIN=${APP_DOMAIN:-${FRONTEND_HOST%:*}}
-export APP_PORT=${APP_PORT:-${FRONTEND_HOST##*:}}
+export APP_DOMAIN=${APP_DOMAIN}
+export APP_PORT=${APP_PORT}
 
 # strip https:// or https:// from domain if necessary
 APP_DOMAIN=${APP_DOMAIN##*://}
 
-echo using app: ${APP_DOMAIN} with port: ${APP_PORT} frontend host: ${FRONTEND_HOST}
+echo using app: ${APP_DOMAIN} with port: ${APP_PORT}
 
 exec caddy run --config Caddyfile --adapter caddyfile 2>&1
