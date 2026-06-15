b0b23d131032188c8cca0b53d7fa6bdb5185c1e4
niklasbaumgardner <niklasbaumgardner@gmail.com>
2026-06-15 15:13:57 -0400

    copy webawesome styles

--- finapp/static/js/sentry.mjs
+++ finapp/static/js/sentry.mjs
@@ -1,7 +1,7 @@
 if (window.Sentry) {
   Sentry.onLoad(function () {
     Sentry.init({
-      release: "nbbudgetfront@3.0.2",
+      release: "nbbudgetfront@3.0.3",
       environment: "production",
       integrations: [Sentry.browserTracingIntegration()],
       tracesSampleRate: 1.0,
