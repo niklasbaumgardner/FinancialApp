91625d47fe33e28e08dc5447839a675e33dc592d
niklasbaumgardner <niklasbaumgardner@gmail.com>
2026-06-30 12:49:27 -0400

    overflow hidden wa-dialog

--- finapp/static/dist/esbuild/sentry.BLUVFVMW.mjs
+++
@@ -1,2 +0,0 @@
-window.Sentry&&Sentry.onLoad(function(){Sentry.init({release:"nbbudgetfront@3.0.7",environment:"production",integrations:[Sentry.browserTracingIntegration()],tracesSampleRate:1,sendDefaultPii:!0})});
-//# sourceMappingURL=sentry.BLUVFVMW.mjs.map
---
+++ finapp/static/dist/esbuild/sentry.MMVJTFNR.mjs
@@ -0,0 +1,2 @@
+window.Sentry&&Sentry.onLoad(function(){Sentry.init({release:"nbbudgetfront@3.0.8",environment:"production",integrations:[Sentry.browserTracingIntegration()],tracesSampleRate:1,sendDefaultPii:!0})});
+//# sourceMappingURL=sentry.MMVJTFNR.mjs.map
