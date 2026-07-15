1c4f90b81a736d24990b455bc34864cb40fd9aa7
niklasbaumgardner <niklasbaumgardner@gmail.com>
2026-07-15 13:05:43 -0400

    build again

---
+++ finapp/static/dist/esbuild/sentry.FGTNG57K.mjs
@@ -0,0 +1,2 @@
+window.Sentry&&Sentry.onLoad(function(){Sentry.init({release:"nbbudgetfront@3.0.11",environment:"production",integrations:[Sentry.browserTracingIntegration()],tracesSampleRate:1,sendDefaultPii:!0})});
+//# sourceMappingURL=sentry.FGTNG57K.mjs.map
