9593e574930b728372f82dd5d32b5d703dc005d4
niklasbaumgardner <niklasbaumgardner@gmail.com>
2026-09-03 17:33:34 -0400

    raise data grid css

---
+++ finapp/static/dist/esbuild/sentry.V3FNEFOK.mjs
@@ -0,0 +1,2 @@
+window.Sentry&&Sentry.onLoad(function(){Sentry.init({release:"nbbudgetfront@3.0.23",environment:"production",integrations:[Sentry.browserTracingIntegration()],tracesSampleRate:1,sendDefaultPii:!0})});
+//# sourceMappingURL=sentry.V3FNEFOK.mjs.map
--- finapp/static/js/sentry.mjs
+++ finapp/static/js/sentry.mjs
@@ -1,7 +1,7 @@
 if (window.Sentry) {
   Sentry.onLoad(function () {
     Sentry.init({
-      release: "nbbudgetfront@3.0.22",
+      release: "nbbudgetfront@3.0.23",
       environment: "production",
       integrations: [Sentry.browserTracingIntegration()],
       tracesSampleRate: 1.0,
