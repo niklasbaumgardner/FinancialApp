2b2aefa04ac4c45bf769740a14cd70a10a2a5af3
niklasbaumgardner <niklasbaumgardner@gmail.com>
2026-09-15 16:34:18 -0400

    better data grid styling

---
+++ finapp/static/dist/esbuild/sentry.PQLY3T4N.mjs
@@ -0,0 +1,2 @@
+window.Sentry&&Sentry.onLoad(function(){Sentry.init({release:"nbbudgetfront@3.0.37",environment:"production",integrations:[Sentry.browserTracingIntegration()],tracesSampleRate:1,sendDefaultPii:!0})});
+//# sourceMappingURL=sentry.PQLY3T4N.mjs.map
--- finapp/static/js/sentry.mjs
+++ finapp/static/js/sentry.mjs
@@ -1,7 +1,7 @@
 if (window.Sentry) {
   Sentry.onLoad(function () {
     Sentry.init({
-      release: "nbbudgetfront@3.0.36",
+      release: "nbbudgetfront@3.0.37",
       environment: "production",
       integrations: [Sentry.browserTracingIntegration()],
       tracesSampleRate: 1.0,
