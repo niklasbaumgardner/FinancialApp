d1990af43a55f9b713db0347ca052ab0a53a05e5
niklasbaumgardner <niklasbaumgardner@gmail.com>
2026-09-16 15:28:40 -0400

    better nav bars in mobile and desktop

--- finapp/static/dist/esbuild/sentry.WKRJ5JJS.mjs
+++
@@ -1,2 +0,0 @@
-window.Sentry&&Sentry.onLoad(function(){Sentry.init({release:"nbbudgetfront@3.0.44",environment:"production",integrations:[Sentry.browserTracingIntegration()],tracesSampleRate:1,sendDefaultPii:!0})});
-//# sourceMappingURL=sentry.WKRJ5JJS.mjs.map
